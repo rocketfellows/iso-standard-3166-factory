bf480779e981928d9528acb7d447249123b6e2b7
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-01-14 15:51:57 +0400

    Covered success create country by code.

--- tests/unit/CountryFactoryTest.php
+++ tests/unit/CountryFactoryTest.php
@@ -2,6 +2,7 @@
 
 namespace rocketfellows\ISOStandard3166Factory\tests\unit;
 
+use arslanimamutdinov\ISOStandard3166\Country;
 use PHPUnit\Framework\TestCase;
 
 class CountryFactoryTest extends TestCase
@@ -16,5 +17,47 @@ class CountryFactoryTest extends TestCase
         parent::setUp();
 
         $this->countryFactory = new CountryFactory();
+    }
+
+    /**
+     * @dataProvider getSuccessCreateCountryByCodeProvidedData
+     */
+    public function testSuccessCreateCountryByCode(string $code, Country $expectedCountry): void
+    {
+        $this->assertEquals($expectedCountry, $this->countryFactory->createByCode($code));
+    }
+
+    public function getSuccessCreateCountryByCodeProvidedData(): array
+    {
+        return [
+            'alpha2 country code in upper case' => [
+                'code' => 'AT',
+                'expectedCountry' => new Country('Austria', 'AT', 'AUT', '040'),
+            ],
+            'alpha2 country code in lower case' => [
+                'code' => 'at',
+                'expectedCountry' => new Country('Austria', 'AT', 'AUT', '040'),
+            ],
+            'alpha2 country code in lower and upper case' => [
+                'code' => 'aT',
+                'expectedCountry' => new Country('Austria', 'AT', 'AUT', '040'),
+            ],
+            'alpha3 country code in upper case' => [
+                'code' => 'AUT',
+                'expectedCountry' => new Country('Austria', 'AT', 'AUT', '040'),
+            ],
+            'alpha3 country code in lower case' => [
+                'code' => 'aut',
+                'expectedCountry' => new Country('Austria', 'AT', 'AUT', '040'),
+            ],
+            'alpha3 country code in lower and upper case' => [
+                'code' => 'AuT',
+                'expectedCountry' => new Country('Austria', 'AT', 'AUT', '040'),
+            ],
+            'numeric code' => [
+                'code' => '040',
+                'expectedCountry' => new Country('Austria', 'AT', 'AUT', '040'),
+            ],
+        ];
     }
 }
