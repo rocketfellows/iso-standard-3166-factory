bc31f4e54be275fa0dc74988686ca7d05c7666a6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-01-14 16:06:13 +0400

    Import factory.

--- tests/unit/CountryFactoryTest.php
+++ tests/unit/CountryFactoryTest.php
@@ -4,6 +4,7 @@ namespace rocketfellows\ISOStandard3166Factory\tests\unit;
 
 use arslanimamutdinov\ISOStandard3166\Country;
 use PHPUnit\Framework\TestCase;
+use rocketfellows\ISOStandard3166Factory\CountryFactory;
 use rocketfellows\ISOStandard3166Factory\exceptions\EmptyCountryCodeException;
 use rocketfellows\ISOStandard3166Factory\exceptions\UnknownCountryCodeException;
 
